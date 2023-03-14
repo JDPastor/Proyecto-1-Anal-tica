19f8aa47bf010545916ac9fe698c646371335d8f
JDPastor <124736461+JDPastor@users.noreply.github.com>
2023-03-14 14:46:51 -0500

    Add files via upload

---
+++ Preprocesamiento final.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 12:42:03 2023
+
+@author: juand
+"""
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(r'C:\Users\juand\OneDrive\Documentos\Analitica computacional\Proyecto 1\processed.cleveland.data', header=None,names=["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal","num"])
+
+
+#Análisis exploratorio de los datos
+df.replace('?', np.nan, inplace=True)
+print(df)
+print(pd.isnull(df).any())
+print(df.shape)
+df.dropna(inplace=True)
+descripcion=df.describe(include='all')
+print(df['ca'].hist())
+
+
+df['ca'] = df['ca'].astype('category')
+df['thal'] = df['thal'].astype('category')
+df['exang'] = df['exang'].astype('category')
+df['slope'] = df['slope'].astype('category')
+df['fbs'] = df['fbs'].astype('category')
+df['cp'] = df['cp'].astype('category')
+df['sex'] = df['sex'].astype('category')
+df['oldepeak'] = df['oldpeak'].astype('category')
+df['restecg'] = df['restecg'].astype('category')
+df['num'] = df['num'].astype('category')
+print(df.dtypes)
+
+descripcion=df.describe(include='all')
+descripcion.to_csv('Descripcion1',index = True)
+# Plot a histogram for each feature in the dataframe
+df.hist(bins=50, figsize=(30,30))
+plt.show()
+
+
+# Plot a heatmap to visualize the correlation between features
+plt.figure(figsize=(10,10))
+sns.heatmap(df.corr(), annot=False)
+plt.show()
+
+#Binarización de variables
+df['cp'] = df['cp'].apply(lambda x: 0 if x == 4 else 1)
+df['restecg'] = df['restecg'].apply(lambda x: 1 if x >= 1 else 0)
+df['trestbps'] = df['trestbps'].apply(lambda x: 1 if x >= 120 else 0)
+df['chol'] = df['chol'].apply(lambda x: 1 if x >= 240 else 0)
+for i in df.index:
+    if df.loc[i, 'thalach'] >= (220 - df.loc[i, 'age']):
+        df.loc[i, 'thalach'] = 1
+    else:
+        df.loc[i, 'thalach'] = 0
+df['age'] = df['age'].apply(lambda x: 1 if x >= 45 else 0)
+df.drop('oldpeak',axis=1, inplace=True)
+df['slope'] = df['slope'].apply(lambda x: 1 if x == 3 else 0)
+df['thal'] = df['thal'].apply(lambda x: 0 if x == '3.0' else 1)
+df['ca'] = df['ca'].apply(lambda x: 0 if x == '0.0' else 1)
+df['num']=df['num'].apply(lambda x:0 if x==0 else 1)
+
+df = df.drop('ca',axis = 1)
+
+df.to_csv('datosFinal',index = False)
+
+
